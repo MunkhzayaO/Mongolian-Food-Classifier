2e43bb0ced176eb29b9c0d6bc6882212a5cf0a7f
MunkhzayaO <144086480+MunkhzayaO@users.noreply.github.com>
2024-02-21 18:19:08 +0800

    Create monfood.py

---
+++ monfood.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from fastai.vision.all import *
+import gdown
+
+st.markdown("""# Mongolian Food Classifier
+
+Mongolia has four traditional foods: Buuz, Huushuur, Niislel Salad, Tsuivan. This app allows you to upload an image of one of these four foods and the connected model will classify it for you. Upload an image and try it out!.""")
+
+st.markdown("""### Upload your image here""")
+
+image_file = st.file_uploader("Image Uploader", type=["png","jpg","jpeg"])
+
+## Model Loading Section
+model_path = Path("export.pkl")
+
+if not model_path.exists():
+    with st.spinner("Downloading model... this may take awhile! \n Don't stop it!"):
+        url = 'https://colab.research.google.com/uc?id=17bk4N8X4OuNWaLyzJxxt_73P18EWgh_R'
+        output = 'export.pkl'
+        gdown.download(url, output, quiet=False)
+    learn_inf = load_learner('export.pkl')
+else:
+    learn_inf = load_learner('export.pkl')
+
+col1, col2 = st.columns(2)
+if image_file is not None:
+    img = PILImage.create(image_file)
+    pred, pred_idx, probs = learn_inf.predict(img)
+
+    with col1:
+        st.markdown(f"""### Predicted Food: {pred.capitalize()}""")
+        st.markdown(f"""### Probability: {round(max(probs.tolist()), 3) * 100}%""")
+    with col2:
+        st.image(img, width=300)
